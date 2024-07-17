4652621d97d53a62a4097b0102b7e73e27e1aa01
Syed Shahan Hussain <shahanhussain9105@gmail.com>
2024-07-17 20:02:29 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        fira: ["'Fira Sans', sans-serif"],
+        pacifico: [" 'Pacifico', cursive"],
+        fredoka: ["'Fredoka One', cursive"],
+        allerta: ["'Allerta', sans-serif"],
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        // mobile: "url('./public/olxlanding.png')",
+      },
+      animation: {
+        'bounce-slow': 'bounce 2s linear infinite',
+        'slow-bounce': 'slow-bounce 2s ease-in-out infinite',
+        'bounce-avatar': 'bounce-avatar 2s ease-in-out infinite',
+      },
+      keyframes: {
+        'slow-bounce': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+        'bounce-avatar': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
       },
     },
   },
